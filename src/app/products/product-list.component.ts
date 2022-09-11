c357793866e9c42c70b679f1a10bd029430c73be
helmigandi <sugandihelmi@gmail.com>
2022-09-11 11:23:32 +0700

    add binding

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,31 +1,39 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-products',
-  templateUrl: './product-list.component.html'
+  templateUrl: './product-list.component.html',
 })
 export class ProductListComponent {
-  pageTitle: string = 'Product List!';
+  pageTitle = 'Product List!';
+  imageWidth = 50;
+  imageMargin = 2;
+  showImage = false;
+  listFilter = 'cart';
   products: any[] = [
     {
-      "id": 1,
-      "productName": "Macbook Pro M2",
-      "productCode": "N220001",
-      "releaseDate": "May 21, 2022",
-      "description": "The new Macbook Pro with M2 chip",
-      "price": 1999.99,
-      "starRating": 4.8,
-      "imageUrl": "assets/image/macbook-pro-m2.jpg"
+      id: 1,
+      productName: 'Macbook Pro M2',
+      productCode: 'N220001',
+      releaseDate: 'May 21, 2022',
+      description: 'The new Macbook Pro with M2 chip',
+      price: 25_000_000,
+      starRating: 4.8,
+      imageUrl: 'assets/image/mbp2.jpg',
     },
     {
-      "id": 2,
-      "productName": "Dell XPS 13 Plus",
-      "productCode": "N220002",
-      "releaseDate": "August 10, 2022",
-      "description": "The new model of XPS 13",
-      "price": 1799.19,
-      "starRating": 4.1,
-      "imageUrl": "assets/image/xps-13-plus.jpg"
-    }
+      id: 2,
+      productName: 'Dell XPS 13 Plus',
+      productCode: 'N220002',
+      releaseDate: 'August 10, 2022',
+      description: 'The new model of XPS 13',
+      price: 19_999_999,
+      starRating: 4.1,
+      imageUrl: 'assets/image/dell-xps.jpg',
+    },
   ];
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
 }
