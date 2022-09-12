56b2d382c8b30769debcd46f9f506f0fd2ea8893
helmigandi <sugandihelmi@gmail.com>
2022-09-12 16:06:46 +0700

    add basic routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { ProductListComponent } from './products/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'products', component: ProductListComponent},
+  {path: 'products/:id', component: ProductDetailComponent},
+  {path: 'welcome', component: WelcomeComponent},
+  {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+  {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+  // {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,40 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
   template: `
-    <div>
-      <h1>{{ pageTitle }}</h1>
-      <pm-products></pm-products>
+    <nav class="navbar navbar-expand navbar-light bg-light">
+      <a class="navbar-brand">{{ pageTitle }}</a>
+      <ul class="navbar-nav">
+        <a
+          class="nav-link"
+          routerLink="/welcome"
+          [ngClass]="{ active: isValue === 1 }"
+          (click)="toggle(1)"
+          )
+          >Home</a
+        >
+        <a
+          class="nav-link"
+          routerLink="/products"
+          [ngClass]="{ active: isValue === 2 }"
+          (click)="toggle(2)"
+          )
+          >Product list</a
+        >
+        <!-- <a class="nav-link" routerLink="/products">Product list</a> -->
+      </ul>
+    </nav>
+    <div class="container">
+      <router-outlet></router-outlet>
     </div>
-  `
+  `,
 })
 export class AppComponent {
   pageTitle: string = 'Helmi Product Management';
+  isValue: number = 0;
+
+  toggle(num: number): void {
+    this.isValue = num;
+  }
 }
