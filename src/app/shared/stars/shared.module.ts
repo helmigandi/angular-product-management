6ddc1cd7ea6bcac8202ae795d2bb1a2a023e1f00
helmigandi <sugandihelmi@gmail.com>
2022-09-13 09:37:57 +0700

    add angular module

---
+++ src/app/shared/stars/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StarComponent } from './star.component';
+import { FormsModule } from '@angular/forms';
+
+/**
+ *  ng g m shared/stars/shared --flat -m products/product.module
+ */
+@NgModule({
+  declarations: [StarComponent],
+  imports: [CommonModule],
+  exports: [CommonModule, FormsModule, StarComponent],
+})
+export class SharedModule {}
