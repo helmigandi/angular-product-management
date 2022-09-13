9c03d9fdabff9b4d9c5dd1913c9c76f68ffaafd6
helmigandi <sugandihelmi@gmail.com>
2022-09-13 11:20:28 +0700

    add cli command

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'pm-product-detail',
@@ -9,26 +10,31 @@ import { IProduct } from './product';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product detail';
+  errorMessage: string = '';
   product: IProduct | undefined;
 
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private productService: ProductService
+  ) {}
 
   onBack(): void {
     this.router.navigate(['/products']);
   }
 
+  getProduct(id: number): void {
+    this.productService.getProduct(id).subscribe({
+      next: (product) => (this.product = product),
+      error: (err) => (this.errorMessage = err),
+    });
+  }
+
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     this.pageTitle += ` ${id}`;
-    this.product = {
-      productId: 1,
-      productName: 'Leaf Rake',
-      productCode: 'GDN-0011',
-      releaseDate: 'March 19, 2021',
-      description: 'Leaf rake with 48-inch wooden handle.',
-      price: 19.95,
-      starRating: 3.2,
-      imageUrl: 'assets/image/mbp2.jpg',
-    };
+    if (id) {
+      this.getProduct(Number(id));
+    }
   }
 }
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, Observable, tap, throwError } from 'rxjs';
+import { catchError, map, Observable, tap, throwError } from 'rxjs';
 import { IProduct } from './product';
 
 @Injectable({
@@ -17,6 +17,16 @@ export class ProductService {
       .pipe(
         tap(data => console.log('All: ', JSON.stringify(data))),
         catchError(this.handleError)
+      );
+  }
+
+  // Get one product
+  // Since we are working with a json file, we can only retrieve all products
+  // So retrieve all products and then find the one we want using 'map'
+  getProduct(id: number): Observable<IProduct | undefined> {
+    return this.getProducts()
+      .pipe(
+        map((products: IProduct[]) => products.find(p => p.productId === id))
       );
   }
 
